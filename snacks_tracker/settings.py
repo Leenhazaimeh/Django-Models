7bb313994e74c3476c62feeca6a6058394090298
Leen Hazaimeh <leenhazaimeh94@gmail.coom>
2021-08-30 22:08:13 +0300

    fineshed the codeing

--- snacks_tracker/settings.py
+++ snacks_tracker/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+     'snacks',
 ]
 
 MIDDLEWARE = [
@@ -47,7 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'snacks',
+   
 ]
 
 ROOT_URLCONF = 'snacks_tracker.urls'
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'snacks_tracker.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates' ],
+        'DIRS': [ ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
